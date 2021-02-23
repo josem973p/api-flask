6764e803f694cd9bca53c1f269bb6b375c9fc6a4
Jose Manuel Castaneda <josscas1997@gmail.com>
2021-02-23 17:11:45 -0600

    prueba del metodo post para añadir alumno

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #importaciones
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 
 # conexion db e inicio app
@@ -9,8 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:418103812@localhos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-class alunmno(db.Model):
+class alumno(db.Model):
    
     idalumnos = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(45))
@@ -18,7 +20,7 @@ class alunmno(db.Model):
     edad =db.Column(db.Integer)
     carreraId = db.Column(db.Integer)
     materiaId = db.Column(db.Integer)
-    
+
 
     def __init__(self, nombre, apellido,edad,carreraId, materiaId):
         self.nombre = nombre
@@ -29,11 +31,31 @@ class alunmno(db.Model):
 
 db.create_all()
 
+class TaskSchema(ma.Schema):
+    class Meta:
+        fields = ('nombre', 'apellido', 'edad','carreraId','materiaId')
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
+
 #endpoints
 
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'mensaje': 'endpint raiz'})
+
+@app.route('/post', methods=['POST'])
+def createAlumnos():
+    nombre= request.json['nombre']
+    apellido = request.json['apellido']
+    edad = request.json['edad']
+    carreraId = request.json['carreraId']
+    materiaId = request.json['materiaId']
+
+    newAlumno = alumno(nombre,apellido, edad, carreraId,materiaId)
+    db.session.add(newAlumno)
+    db.session.commit()
+    return task_schema.jsonify(newAlumno)
 
 
 #main
