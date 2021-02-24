daf70b07b34863dc67372d6710c1137f9effde97
Jose Manuel Castaneda <josscas1997@gmail.com>
2021-02-23 23:20:02 -0600

    se añadio el mapeo de las tablas faltantes de la bd

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from flask_marshmallow import Marshmallow
 
 # conexion db e inicio app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:418103812@localhost/api-flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:418103812@flask.cvowhhzoj0ol.us-east-2.rds.amazonaws.com/api_flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
+'''______________________________________________________________________________________'''
 class alumno(db.Model):
    
     idalumnos = db.Column(db.Integer, primary_key=True)
@@ -31,20 +31,69 @@ class alumno(db.Model):
 
 db.create_all()
 
-class TaskSchema(ma.Schema):
+class alumnochema(ma.Schema):
     class Meta:
         fields = ('nombre', 'apellido', 'edad','carreraId','materiaId')
 
-task_schema = TaskSchema()
-tasks_schema = TaskSchema(many=True)
+alumno_schema = alumnochema()
+alumno_schema = alumnochema(many=True)
+
+'''______________________________________________________________________________________'''
+
+class materia(db.Model):
+   
+    idmateria = db.Column(db.Integer, primary_key=True)
+    materia = db.Column(db.String(45))
+  
+
+
+    def __init__(self, materia):
+        self.materia = materia
+     
+
+db.create_all()
+
+class materiaSchema(ma.Schema):
+    class Meta:
+        fields = ('materia',)
+
+materia_schema = materiaSchema()
+materias_schema = materiaSchema(many=True)
+
+'''______________________________________________________________________________________'''
+
+'''______________________________________________________________________________________'''
+
+class carrera(db.Model):
+   
+    idcarrera = db.Column(db.Integer, primary_key=True)
+    carrera = db.Column(db.String(45))
+  
+
+
+    def __init__(self, carrera):
+        self.carrera = carrera
+     
+
+db.create_all()
+
+class carreraSchema(ma.Schema):
+    class Meta:
+        fields = ('carrera',)
+
+carrera_schema = carreraSchema()
+carreras_schema = carreraSchema(many=True)
+
+'''______________________________________________________________________________________'''
+
 
 #endpoints
 
 @app.route('/', methods=['GET'])
 def index():
-    return jsonify({'mensaje': 'endpint raiz'})
+    return jsonify({'mensaje': 'endpoint raiz'})
 
-@app.route('/post', methods=['POST'])
+@app.route('/postAlumno', methods=['POST'])
 def createAlumnos():
     nombre= request.json['nombre']
     apellido = request.json['apellido']
@@ -55,7 +104,22 @@ def createAlumnos():
     newAlumno = alumno(nombre,apellido, edad, carreraId,materiaId)
     db.session.add(newAlumno)
     db.session.commit()
-    return task_schema.jsonify(newAlumno)
+    return alumno_schema.jsonify(newAlumno)
+
+@app.route('/postCarrera', methods=['POST'])
+def createCarrera():
+    car= request.json['carrera']
+    newCarrera = carrera(car)
+    db.session.add(newCarrera)
+    db.session.commit()
+    return carrera_schema.jsonify(newCarrera)
+
+
+@app.route('/table/<tabla>', methods=['GET'])
+def get_tasks(tabla):
+  data = tabla.query.all()
+  result = tasks_schema.dump(data)
+  return jsonify(result)
 
 
 #main
